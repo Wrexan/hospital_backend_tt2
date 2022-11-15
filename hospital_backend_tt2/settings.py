2066a119cd25419baea5ae86f938e2b71729eb03
Wrexan <fwrexan@gmail.com>
2022-11-15 12:52:20 +0200

    created apps

--- hospital_backend_tt2/settings.py
+++ hospital_backend_tt2/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointments',
+    'clients',
+    'locations',
+    'schedules',
+    'staff',
+    'workers',
 ]
 
 MIDDLEWARE = [
