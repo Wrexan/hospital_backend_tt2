19fda4d82a0185d483500b5fd2e1b966a7920da1
Wrexan <fwrexan@gmail.com>
2022-11-21 11:47:51 +0200

    refactoring of create_staff.py add "client" to create_staff.py

--- users/management/commands/create_staff.py
+++ users/management/commands/create_staff.py
@@ -2,21 +2,50 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 from users.models import User
 
+staff = (
+    {
+        'name': 'Superuser',
+        'username': 'admin',
+        'password': 'admin',
+        'group': '',
+        'superuser': True
+    },
+    {
+        'name': 'Manager',
+        'username': 'manager',
+        'password': '123',
+        'group': 'managers',
+        'superuser': False
+    },
+    {
+        'name': 'Administrator',
+        'username': 'administrator',
+        'password': '123',
+        'group': 'admins',
+        'superuser': False
+    },
+    {
+        'name': 'Client',
+        'username': 'client',
+        'password': '123',
+        'group': 'clients',
+        'superuser': False
+    },
+)
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        super_user = User.objects.create_superuser('admin', '', 'admin')
-        super_user.save()
-        print("Superuser created. USERNAME: 'admin' PASSWORD: 'admin'")
-
-        manager = User.objects.create_user('manager', '', '123')
-        manager_group = Group.objects.get(name='managers')
-        manager.save()
-        manager_group.user_set.add(manager)
-        print("Manager created. USERNAME: 'manager' PASSWORD: '123'")
-
-        administrator = User.objects.create_user('administrator', '', '123')
-        administrator_group = Group.objects.get(name='admins')
-        administrator.save()
-        administrator_group.user_set.add(administrator)
-        print("Administrator created. USERNAME: 'administrator' PASSWORD: '123'")
+        for user_data in staff:
+            user_exist = User.objects.filter(username=user_data['username'])
+            if user_exist.exists():
+                continue
+            if user_data['superuser']:
+                user = User.objects.create_superuser(user_data['username'], '', user_data['password'])
+                user.save()
+            else:
+                user = User.objects.create_user(user_data['username'], '', user_data['password'])
+                user.save()
+                user_group = Group.objects.get(name=user_data['group'])
+                user_group.user_set.add(user)
+            print(f"{user_data['name']} created. USERNAME: {user_data['username']} PASSWORD: {user_data['password']}")
